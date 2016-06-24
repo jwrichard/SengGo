e3ffea8f1d3f5cad01afe84cdf3f2a617ed144cc
Justin Richard <justinr@uvic.ca>
2016-06-23 17:17:17 -0700

    latest updates

--- server.js
+++ server.js
@@ -23,24 +23,37 @@ app.use(bodyParser.urlencoded({
 
 // Deliver our home page 
 app.get('/', function (req, res) {
-	console.log("Test");
+
+	// Check get params for messages to display
+	var s = "", e = "";
+	switch(req.query.s){
+		case "1": s = '<div class="alert alert-success" role="alert">Welcome back! Succesfully logged in.</div>';
+				break;
+		default: break;
+	}
+	switch(req.query.e){
+		case "1": e = '<div class="alert alert-danger" role="alert">Oops! We\'ve encountered an error with the system. Please try again later.</div>';
+				break;
+		default: break;
+	}
+
+	console.log(s);
 
 	var user = checkLogin();
 	console.log("User is logged in: "+user);
 	if(user != ""){
 		var page = fs.readFileSync("views/index.html", "utf8"); // bring in the HTML file
-		var html = mustache.to_html(page, {loginstatus: '<div><p class="navbar-form navbar-right loginstatus">Welcome back! Logged in as: <span class="secondaryWord">'+user+'</span></p></div>'}); // replace all of the data
+		var html = mustache.to_html(page, {loginstatus: '<div><p class="navbar-form navbar-right loginstatus">Welcome back! Logged in as: <span class="secondaryWord">'+user+'</span></p></div>', error: e, success: s}); // replace all of the data
 		res.send(html);
 	} else {
 		var page = fs.readFileSync("views/index.html", "utf8"); // bring in the HTML file
-		var html = mustache.to_html(page, {loginstatus: '<form class="navbar-form navbar-right" action="/actionLogin" method="post"><div class="form-group"><input type="text" placeholder="Username" class="form-control"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control"></div><button type="submit" class="btn btn-primary">Sign in</button></form>', authreq: '(Authentication required)'}); // replace all of the data
+		var html = mustache.to_html(page, {loginstatus: '<form class="navbar-form navbar-right" action="/actionLogin" method="post"><div class="form-group"><input type="text" placeholder="Username" class="form-control"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control"></div><button type="submit" class="btn btn-primary">Sign in</button></form>', authreq: '(Authentication required)', error: e, success: s}); // replace all of the data
 		res.send(html);
 	}
 	
 })
 
 // About page
-/*
 app.get('/about', function (req, res) {
    res.sendFile(__dirname + "/" + "about.html");
 })
@@ -59,26 +72,45 @@ app.get('/login', function (req, res) {
 app.post('/actionLogin', function (req, res) {
 
 	var username = req.body.username;
-	var password = req.body.p;
+	var password = req.body.p
 
 	if(doLogin(username, password)){
-		res.sendFile(__dirname + "/" + "index.html");
+		res.redirect("/?s=1");
 	} else {
-		res.sendFile(__dirname + "/" + "login.html");
+		res.redirect("/login/?e=1");
 	}
 })
 
 // Register page
 app.get('/register', function (req, res) {
-   res.sendFile(__dirname + "/" + "register.html");
+	var page = fs.readFileSync("views/register.html", "utf8"); // bring in the HTML file
+	var html = mustache.to_html(page, {}); // replace all of the data
+	res.send(html);
+
 })
+
+// Handle registration submission
+app.post('/actionRegister', function (req, res) {
+	// Get post params
+	var username = req.body.username;
+	var password = req.body.p
+
+	// Check to see if username taken
+
+	// If not, create a salted password and a salt
+
+	// Store new user into db
+
+	// Redirect to login page with success message
+})
+
+
 
 // Redirect all unsupported pages to the home page
 app.get('*', function (req, res) {
     //res.redirect('/');
 });
 
-*/
 
 // Listen on default port 
 var server = app.listen(80, function () {
@@ -90,7 +122,7 @@ var server = app.listen(80, function () {
 
 // Functions to be called above
 function doLogin(username, password){
-	return false;
+	return true;
 }
 
 function checkLogin(){
