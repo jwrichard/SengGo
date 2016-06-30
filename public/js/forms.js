2cb5030734b878e656bd3d1b18edbf90b6d20476
Justin Richard <justinr@uvic.ca>
2016-06-30 14:51:20 -0700

    Login/logout now works alongside registration, updated home page menu to ask for board size & other options and POST request

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,4 +1,17 @@
-function formhash(form, password) {
+function formhash(form, username, password) {
+
+    // Check username length
+    if(username.value.length < 4){
+        alert("Username too short! Minimum 4 characters");
+        return false;
+    }
+
+    // Make sure password is long enough
+    if(password.value.length < 6){
+        alert("Password too short! Minimum 6 characters");
+        return false;
+    }
+
     // Create a new element input, this will be our hashed password field. 
     var p = document.createElement("input");
  
@@ -11,6 +24,43 @@ function formhash(form, password) {
     // Make sure the plaintext password doesn't get sent. 
     password.value = "";
  
+    // Finally submit the form. 
+    form.submit();
+}
+
+function regformhash(form, username, password, confirmpass) {
+
+    // Check username length
+    if(username.value.length < 4){
+        alert("Username too short! Minimum 4 characters");
+        return false;
+    }
+
+    // Make sure password is long enough
+    if(password.value.length < 6){
+        alert("Password too short! Minimum 6 characters");
+        return false;
+    }
+
+    // Make sure passwords match
+    if(password.value !== confirmpass.value){
+        alert("Passwords do not match! Please try again.");
+        return false;
+    }
+
+    // Create a new element input, this will be our hashed password field. 
+    var p = document.createElement("input");
+ 
+    // Add the new element to our form. 
+    form.appendChild(p);
+    p.name = "p";
+    p.type = "hidden";
+    p.value = hex_sha512(password.value);
+ 
+    // Make sure the plaintext password doesn't get sent. 
+    password.value = "";
+    confirmpass.value = "";
+
     // Finally submit the form. 
     form.submit();
 }
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mustache = require('mustache');
 var Crypto = require('crypto')
-var fs = require('fs'); // require the filesystem api
+var fs = require('fs');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
 
 // Initialize DB
@@ -14,6 +16,18 @@ var sha = require('./lib/sha512.js');
 
 // Add static files directory
 app.use(express.static("public"));
+app.use(cookieParser())
+
+// Setup sessions and how the IDs are created
+app.use(session({
+  genid: function(req) {
+    return Crypto.randomBytes(32).toString('base64'); // use UUIDs for session IDs 
+  },
+  secret: 'a10758sf31f1f9dsd90192e23smd546912fqsmsffn12',
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 // Used to parse JSON objects from requests
 app.use(bodyParser.json());
@@ -27,7 +41,6 @@ app.use(bodyParser.urlencoded({
 
 // Deliver our home page 
 app.get('/', function (req, res) {
-
 	// Check get params for messages to display
 	var s = "", e = "";
 	switch(req.query.s){
@@ -41,19 +54,26 @@ app.get('/', function (req, res) {
 		default: break;
 	}
 
-
-	var user = checkLogin();
-	console.log("User is logged in: "+user);
+	(req.session.username ? user = req.session.username :  user = '');
+	var pageData;
+	console.log("User logged in: "+user);
 	if(user != ""){
-		var page = fs.readFileSync("views/index.html", "utf8"); // bring in the HTML file
-		var html = mustache.to_html(page, {loginstatus: '<div><p class="navbar-form navbar-right loginstatus">Welcome back! Logged in as: <span class="secondaryWord">'+user+'</span></p></div>', error: e, success: s}); // replace all of the data
-		res.send(html);
+		pageData = {loginstatus: '<div><p class="navbar-form navbar-right loginstatus">Logged in as: <span class="secondaryWord">'+user+'</span> | <a href="/actionLogout">Logout</a></p></div>', 
+					error: e,
+					menu: '',
+					success: s
+				}; // replace all of the data
 	} else {
-		var page = fs.readFileSync("views/index.html", "utf8"); // bring in the HTML file
-		var html = mustache.to_html(page, {loginstatus: '<form class="navbar-form navbar-right" action="/actionLogin" method="post"><div class="form-group"><input type="text" placeholder="Username" class="form-control"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control"></div><button type="submit" class="btn btn-primary">Sign in</button></form>', authreq: '(Authentication required)', error: e, success: s}); // replace all of the data
-		res.send(html);
+		pageData = {loginstatus: '<form class="navbar-form navbar-right" action="/actionLogin" method="post"><div class="form-group"><input type="text" placeholder="Username" class="form-control" id="username" name="username"></div><div class="form-group"><input type="password" placeholder="Password" class="form-control" id="password" name="password"></div><input type="button" class="btn btn-primary" onclick="formhash(this.form, this.form.username, this.form.password);" value="Sign in" /></form>', 
+					authreq: '(Authentication required)',
+					menu: '<li><a href="/login">Sign in</a></li><li><a href="/register">Register</a></li>',
+					error: e,
+					success: s
+				}; // replace all of the data
 	}
-	
+	var page = fs.readFileSync("views/index.html", "utf8"); // bring in the HTML file
+	var html = mustache.to_html(page, pageData); // replace all of the data
+	res.send(html);
 })
 
 // About page
@@ -82,9 +102,13 @@ app.get('/login', function (req, res) {
 	switch(req.query.e){
 		case "1": e = '<div class="alert alert-danger" role="alert">Oops! We\'ve encountered an error with the system. Please try again later.</div>';
 				break;
+		case "2": e = '<div class="alert alert-danger" role="alert">Incorrect credentials provided. Please try again.</div>';
+				break;
+		case "3": e = '<div class="alert alert-danger" role="alert">You must login before playing Online matches.</div>';
+				break;
 		default: break;
 	}
-
+	// Send login page with appropriate error messages
    	var page = fs.readFileSync("views/login.html", "utf8"); // bring in the HTML file
 	var html = mustache.to_html(page, {error: e, success: s}); // replace all of the data
 	res.send(html);
@@ -93,15 +117,43 @@ app.get('/login', function (req, res) {
 // Login action
 app.post('/actionLogin', function (req, res) {
 
+	// Get post data
 	var username = req.body.username;
 	var password = req.body.p
 
-	if(doLogin(username, password)){
-		res.redirect("/?s=1");
-	} else {
-		res.redirect("/login/?e=1");
-	}
+	console.log('Login attempt for user: '+username);
+
+	// Compare with user in DB to see if match
+	db.getQuery('users', {username: username}, function(err, result){
+		console.log(result[0]);
+		if(result.length > 0){
+			// Ok so that user exists, but did we supply right password?
+			// Lets encrypt supplied pass with that users salt and see if matches their pass
+			var salt = result[0].salt;
+			var temp_password = sha.hex_sha512(password + salt);
+
+			// Make the comparison
+			if(temp_password === result[0].password){
+				// Success! Set session and redirect
+				req.session.username = username;
+				res.redirect("/?s=1");
+			} else {
+				// WRONG! Redirect
+				res.redirect("/login?e=2");
+			}
+		} else {
+			// No user with that Id exists
+			res.redirect("/login?e=2");
+		}
+	});
 })
+
+// Logout action
+app.get('/actionLogout', function (req, res) {
+	req.session.username = '';
+	res.redirect('/');
+})
+
 
 // Register page
 app.get('/register', function (req, res) {
@@ -182,15 +234,3 @@ var server = app.listen(80, function () {
   console.log("Server running at 127.0.0.1.");
   db.connect(function(){console.log("Ready to serve requests.");});
 })
-
-
-
-
-// Functions to be called above
-function doLogin(username, password){
-	return true;
-}
-
-function checkLogin(){
-	return "";
-}
